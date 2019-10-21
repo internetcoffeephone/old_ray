Fixed nan mean calculation in ray metrics.

python/ray/rllib/evaluation/metrics.py
@@ -89,7 +89,7 @@ def summarize_episodes(episodes, new_episodes, num_dropped):
         policy_rewards[policy_id] = np.mean(rewards)
 
     for k, v_list in custom_metrics.copy().items():
-        custom_metrics[k + "_mean"] = np.mean(v_list)
+        custom_metrics[k + "_mean"] = np.nanmean(v_list)
         filt = [v for v in v_list if not np.isnan(v)]
         if filt:
             custom_metrics[k + "_min"] = np.min(filt)